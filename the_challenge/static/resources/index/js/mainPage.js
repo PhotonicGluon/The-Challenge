9f4e9ad8aec0e5f9ffff48446f0b2236b9dcd3e3
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2020-10-20 23:15:07 +0800

    Add Navigation Bar to the Main Page
    Also updated the licenses page to reflect the new items added to the project.
    
    Will be moving onto beta releases as finalisation of the features of The Challenge 1.0.0 will occur very soon.

--- the_challenge/static/resources/index/js/mainPage.js
+++ the_challenge/static/resources/index/js/mainPage.js
@@ -77,4 +77,34 @@ $(document).ready(() => {
             document.getElementById("start-button").disabled = false;
         }
     });
+
+    // Handle sticky navigation
+    $(".information-box").waypoint((direction) => {
+        if (direction === "down") {
+            $("nav").addClass("sticky")
+        } else {
+            $("nav").removeClass("sticky")
+        }
+    });
+
+    // Scroll handlers
+    $("#js--scroll-to-practice-questions").click(() => {
+        $('html, body').animate({scrollTop: $("#js--practice-questions").offset().top - 75});
+    });
+
+    // Handle clicking of menu icon
+    $(".nav-icon").click(function () {
+        let nav = $(".main-nav");
+        let icon = $(".nav-icon i");
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass("ion-navicon-round")) {
+            icon.addClass("ion-close-round");
+            icon.removeClass("ion-navicon-round");
+        } else {
+            icon.addClass("ion-navicon-round");
+            icon.removeClass("ion-close-round");
+        }
+    });
 });
--- the_challenge/version.py
+++ the_challenge/version.py
@@ -1,1 +1,1 @@
-__version__ = "1.0.0a11.dev8"
+__version__ = "1.0.0a12.dev1"
